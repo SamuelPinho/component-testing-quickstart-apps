b6fbc57399832f249a945c5ff48ef7f91e5120a7
Samuel Monteiro <samuelmonteiro.pinho@gmail.com>
2022-11-30 20:24:03 -0300

    Update Stepper.jsx

--- react/my-awesome-app/src/components/Stepper.jsx
+++ react/my-awesome-app/src/components/Stepper.jsx
@@ -1,29 +1,10 @@
 import { useState } from 'react';
 
 export default function Stepper({ initial = 0, onChange = () => {} }) {
-  const [count, setCount] = useState(initial);
-
-  const handleIncrement = () => {
-    const newCount = count + 1;
-    setCount(newCount);
-    onChange(newCount);
-  };
-
-  const handleDecrement = () => {
-    const newCount = count - 1;
-    setCount(newCount);
-    onChange(newCount);
-  };
-
   return (
-    <div>
-      <button data-cy="decrement" onClick={handleDecrement}>
-        -
-      </button>
-      <span data-cy="counter">{count}</span>
-      <button data-cy="increment" onClick={handleIncrement}>
-        +
-      </button>
-    </div>
+    <>
+      <label>This is a custom label ~!@#$%^&*()_+=-[]{}\|:",./? UID</label>
+      <input id="my-input" type="text" />
+    </>
   );
 }
